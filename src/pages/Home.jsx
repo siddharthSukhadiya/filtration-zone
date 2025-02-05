e2a0324442314a86947277633b3c16d8cbf0154e
Siddharth <117755306+siddharth-1108@users.noreply.github.com>
2025-02-05 23:34:44 +0530

    Add about us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import '../styles/home.css'
-import heroBanner from '../assets/homePage/hero-banner-back.webp'
+import heroBannerImg from '../assets/homePage/hero-banner-back.webp'
+import aboutUsImg from '../assets/homePage/about-us.png'
 
 function Home () {
 
@@ -8,10 +9,10 @@ function Home () {
 		<div className='hero-section'>
             <div className='container'>
                 <div className='hero-wrapper'>
-                	<img src={heroBanner} alt='Hero Banner' />
-                	<div className='heading'>
-                    	<h3>Reliable Filtration & Fitting Solutions for Every Industry</h3>
-                    	<p>Precision-engineered pipeline filters, valve housings, and flange fittings—designed for durability and efficiency in industrial applications.</p>
+                	<img src={heroBannerImg} alt='Hero Banner' />
+                	<div className='text'>
+                    	<h3 className='title'>Reliable Filtration & Fitting Solutions for Every Industry</h3>
+                    	<p className='description'>Precision-engineered pipeline filters, valve housings, and flange fittings—designed for durability and efficiency in industrial applications.</p>
                     </div>
                 </div>
             </div>
@@ -19,6 +20,11 @@ function Home () {
 		<div className='about-us'>
 			<div className='container'>
 				<div className='about-us-wrapper'>
+					<img src={aboutUsImg} alt='About us' />
+					<div className='text'>
+						<h3 className='title'>Leading Work That Saves Everyone's Life</h3>
+					    <p className='description'>Manufacturers of Staainer, Duplex Strainer, Basket Strainer, Y Type Strainer, Self Cleaning Filter</p>
+					</div>
 				</div>
 			</div>
 		</div>
