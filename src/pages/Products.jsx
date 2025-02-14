34c3ca80cc2bfde786cd874fb6e62cedf115dfe6
Siddharth <117755306+siddharth-1108@users.noreply.github.com>
2025-02-14 23:24:47 +0530

    Add product list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,11 @@
 import './styles/products.css'
-import basketFilterImg from '../assets/products/basket-filter-1.jpeg'
-import coneFilterImg from '../assets/products/cone-filter.jpeg'
-import potFilterImg from '../assets/products/pot-filter.jpeg'
+
+import flange1Img from '../assets/products/flange1.webp'
+import flange2Img from '../assets/products/flange2.webp'
+import flange3Img from '../assets/products/flange3.webp'
+import valve1Img from '../assets/products/valve1.webp'
+import valve2Img from '../assets/products/valve2.webp'
+import valve3Img from '../assets/products/valve3.webp'
 
 const Products = () => {
 
@@ -15,23 +19,46 @@ const Products = () => {
 		</section>
 		<section className='product-section'>
 			<div className='container'>
-				<div className='category-title'>
-					<h3 className='section-heading'>Pipeline Strainers & Filters</h3>
-				</div>
-				<div className='product-list'>
-					<div className='product-card'>
-						<img src={basketFilterImg} alt="Basket Filter Img" className='product-image' />
-						<h4>Basket Type</h4>
+				<section className='flanges-section'>
+					<h3 className='section-heading'>Fitting Flanges</h3>
+					<div className='product-list'>
+						<div className='product-card'>
+							<img src={flange1Img} alt="Flange" className='product-image' />
+							<h4>Weld Neck Flange</h4>
+							<p><b>Size:</b> 1/2” to 24”</p>
+						</div>
+						<div className='product-card'>
+							<img src={flange2Img} alt="Flange" className='product-image' />
+							<h4>Threaded Flange</h4>
+							<p><b>Size:</b> 1/2” to 6”</p>
+						</div>
+						<div className='product-card'>
+							<img src={flange3Img} alt="Flange" className='product-image' />
+							<h4>Socket weld Flange</h4>
+							<p><b>Size:</b> 1/2” to 12”</p>
+						</div>
 					</div>
-					<div className='product-card'>
-						<img src={coneFilterImg} alt="Cone Filter Img" className='product-image' />
-						<h4>Cone Shape</h4>
+				</section>
+				<section className='valve-section'>
+					<h3 className='section-heading'>Industrial Valve</h3>
+					<div className='product-list'>
+						<div className='product-card'>
+							<img src={valve1Img} alt="Valve" className='product-image' />
+							<h4>Casted ball valve</h4>
+							<p><b>Material:</b> WCB, CF8, CF8M</p>
+						</div>
+						<div className='product-card'>
+							<img src={valve2Img} alt="Valve" className='product-image' />
+							<h4>Gate Valve</h4>
+							<p><b>Material:</b> WCB, CF8, CF8M, CF3, CF3M</p>
+						</div>
+						<div className='product-card'>
+							<img src={valve3Img} alt="Valve" className='product-image' />
+							<h4>Globe Valve</h4>
+							<p><b>Material:</b> WCB, CF8, CF8M, CF3, CF3M</p>
+						</div>
 					</div>
-					<div className='product-card'>
-						<img src={potFilterImg} alt="Pot Filter Img" className='product-image' />
-						<h4>Pot Type</h4>
-					</div>
-				</div>
+				</section>
 			</div>
 		</section>
 	</div>)
